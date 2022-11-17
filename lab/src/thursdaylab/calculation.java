8fe7dac48723de5b358900ec9fea5ad1d86d39d1
Vijayalakshmi <Vijayalakshmi@VIJI>
2022-11-17 22:54:29 +0530

    commit

---
+++ lab/src/thursdaylab/calculation.java
@@ -0,0 +1,49 @@
+package thursdaylab;
+
+import java.util.Scanner;
+
+public class calculation {
+
+	public static void main(String[] args) {
+		int n1,n2,n3,ch;
+		Scanner sc=new Scanner(System.in);//scanner is to take input from the user
+		System.out.println("Enter 2 numbers: ");
+		n1=sc.nextInt();//first input value is stored in n1
+		n2=sc.nextInt();//second input value is stored in n2
+		if(n1==0 || n2==0)//it checks any one of the input is zero
+		{
+			//is throws to exception zero is not accepted
+			throw new ArithmeticException("Process stopped- zero cannot be accepted");
+		}
+		
+		System.out.println("Enter the opertaor(+,-,*,/):");
+		char op=sc.next().charAt(0);
+		
+		//using switch case to perform mathematical any one at a time
+		switch(op)
+		{
+		case '+': n3=n1+n2;
+		System.out.println("Addition of "+n1+" and "+n2+" is: "+n3);
+		break;
+		
+		case '-':n3=n1-n2;
+		System.out.println("Subtraction of "+n1+" and "+n2+" is: "+n3);
+		break;
+		
+		case '*':n3=n1*n2;
+		System.out.println("Multiplication of "+n1+" and "+n2+" is: "+n3);
+		break;
+		
+		case'/':n3=n1/n2;
+		System.out.println("Division of "+n1+" and "+n2+" is: "+n3);
+		break;
+		
+		default:System.out.println("Invalid operator");
+		break;
+		}
+		
+		
+
+	}
+
+}
---
+++ lab/src/thursdaylab/stringop.java
@@ -0,0 +1,58 @@
+package thursdaylab;
+
+import java.util.Scanner;
+
+public class stringop {
+		public static void main(String[] args) {
+		String str1,str2,str3;//declare datatypes and initilising variables
+		int n,ch;
+		Scanner sc=new Scanner(System.in);//scanner is to take input from the user
+		System.out.println("---String operations---");
+		System.out.println("1.Concatenate of 2 strings");
+		System.out.println("2.Length of the String");
+		System.out.println("3.Compare 2 Strings");
+		System.out.println("4.converting uppercase to lowercase");
+		System.out.println("5.converting lowercase to uppercase");
+		System.out.print("Enter your choice:\n");//asking user which operation to be performed in above listed
+		ch=sc.nextInt();
+		switch(ch)
+		{
+		case 1:System.out.println("Enter 2 strings: ");
+		str1=sc.next();
+		str2=sc.next();
+		str3=str1.concat(str2);
+		System.out.println("Concatenation of 2 strings are:"+str3);
+		break;
+		
+		case 2: System.out.println("Enter the string: ");
+		str1=sc.next();
+		n=str1.length();
+		System.out.println("length of the is :"+n);
+		break;
+		
+		case 3: System.out.println("Enter 2 strings: ");
+		str1=sc.next();
+		str2=sc.next();
+		n=str1.compareTo(str2);
+		System.out.println("compared to strings are:"+n);
+		break;
+		
+		case 4: System.out.println("Enter the string in uppercase:");
+		str1=sc.next();
+		str3=str1.toLowerCase();
+		System.out.println("converted string is: "+str3);
+		break;
+		
+		case 5: System.out.println("Enter the string in lowercase:");
+		str1=sc.next();
+		str3=str1.toUpperCase();
+		System.out.println("converted string is: "+str3);
+		break;
+		
+		default:System.out.println("Invalid choice");
+		
+		
+		}
+		}
+
+}
